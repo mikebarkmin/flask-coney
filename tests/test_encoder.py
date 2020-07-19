99569ac02840d99e2297d844c4146ccbe08f26b7
Mike Barkmin <mike@barkmin.eu>
2020-07-19 11:58:40 +0200

    improve encoder tests

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -1,21 +1,20 @@
-import json
 from uuid import uuid4
+
+import pytest
 
 from flask_coney.encoder import UUIDEncoder
 
 
-def test_encode_with_uuid():
+def test_encode_uuid():
     id = uuid4()
-    pure = {"id": id, "name": "Hi", "list": []}
 
-    encoded = json.dumps(pure, cls=UUIDEncoder)
+    encoded = UUIDEncoder().default(id)
 
     assert str(id) in encoded
 
 
-def test_encode_without_uuid():
-    pure = {"name": "Hi", "list": []}
+def test_encode_not_jsonable():
+    encode = [1, 2, 3, 4]
 
-    encoded = json.dumps(pure, cls=UUIDEncoder)
-
-    assert json.dumps(pure) == encoded
+    with pytest.raises(TypeError):
+        UUIDEncoder().default(encode)
